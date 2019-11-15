8798074ca206fb7b593afe3597eedf1954521e14
Diego <diegoxavier.100@gmail.com>
2019-11-14 23:13:37 -0500

    Vistas de sincronizacion

--- app/src/main/java/com/example/controlriego/Fragment/SincronizacionAutorizacionFragment.java
+++ app/src/main/java/com/example/controlriego/Fragment/SincronizacionAutorizacionFragment.java
@@ -43,14 +43,7 @@ public class SincronizacionAutorizacionFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SincronizacionAutorizacionFragment.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static SincronizacionAutorizacionFragment newInstance(String param1, String param2) {
         SincronizacionAutorizacionFragment fragment = new SincronizacionAutorizacionFragment();
