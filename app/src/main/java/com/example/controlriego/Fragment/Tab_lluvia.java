626c74a5666a8cd2b1ded3c78185e269a3ff32a9
Henryfll <henry.2154@hotmail.com>
2020-01-04 11:50:46 -0500

    Control de lluvia (fragment)

---
+++ app/src/main/java/com/example/controlriego/Fragment/Tab_lluvia.java
@@ -0,0 +1,58 @@
+package com.example.controlriego.Fragment;
+
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.InputType;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import com.example.controlriego.ControlActivity;
+import com.example.controlriego.R;
+
+import java.util.Calendar;
+
+
+public class Tab_lluvia extends Fragment {
+
+    DatePickerDialog picker;
+    EditText txt_fecha;
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view=inflater.inflate(R.layout.fragment_tab_lluvia, container, false);
+        txt_fecha=(EditText) view.findViewById(R.id.fecha_lluvia);
+        txt_fecha.setInputType(InputType.TYPE_NULL);
+        txt_fecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(getActivity(),
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                txt_fecha.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
+
+        return view;
+    }
+
+
+}
