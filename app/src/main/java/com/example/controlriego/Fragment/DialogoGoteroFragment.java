b842e65b9b0b99ed8f3faee9f563a4f0f63ddf67
Henryfll <henry.2154@hotmail.com>
2020-02-12 23:11:41 -0500

    error en dialogfragment

--- app/src/main/java/com/example/controlriego/Fragment/DialogoGoteroFragment.java
+++ app/src/main/java/com/example/controlriego/Fragment/DialogoGoteroFragment.java
@@ -1,20 +1,15 @@
 package com.example.controlriego.Fragment;
 
-import android.app.Dialog;
+
 import android.content.Context;
-import android.net.Uri;
+
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -29,18 +24,15 @@ import java.util.ArrayList;
 public class DialogoGoteroFragment extends DialogFragment {
     private static final String TAG = "MyCustomDialog";
     private long idLote;
-    public interface OnInputListener{
+   /* public interface OnInputListener{
         void sendInput(String input);
     }
-    public OnInputListener mOnInputListener;
+    public OnInputListener mOnInputListener;*/
 
     //widgets
 
     private ListView listView; //  listview donde se cargaran la lista de atractivos
 
-    public ProgressBar progressBar; // Spinner indicador de la carga de atractivos
-
-    private SwipeRefreshLayout swipeContainer; // permite recargar los datos
 
     public ArrayList<GoterosLotesModel> listaGoterosLote = new ArrayList<>(); // Array de atractivos
 
@@ -56,9 +48,7 @@ public class DialogoGoteroFragment extends DialogFragment {
         View view = inflater.inflate(R.layout.dialog_goteros, container, false);
         getDialog().setTitle("Goteros por Lote");
 
-        listView = (ListView) view.findViewById(R.id.listViewGoterosLote);
-
-        progressBar = (ProgressBar) view.findViewById(R.id.progressBar_goterosLote);
+        listView = view.findViewById(R.id.listViewGoterosLote);
 
         mActionOk = view.findViewById(R.id.action_ok);
 
@@ -77,11 +67,11 @@ public class DialogoGoteroFragment extends DialogFragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        try{
+       /* try{
             mOnInputListener = (OnInputListener) getActivity();
         }catch (ClassCastException e){
             Log.e(TAG, "onAttach: ClassCastException: " + e.getMessage() );
-        }
+        }*/
     }
 
     @Override
